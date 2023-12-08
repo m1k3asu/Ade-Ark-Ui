20b510eb362e4e8cca4200925efbd66f4709861f
Michael Martinez <m1k3asu@gmail.com>
2023-12-08 13:03:39 -0600

    mods to fix the update method. More changes needed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,20 @@
 
-import { Component, ViewChild, OnInit, AfterViewInit, ViewEncapsulation } from '@angular/core';
-import { GridComponent, GridColumn, DataAdapter, Smart } from 'smart-webcomponents-angular/grid';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { Grid, GridComponent, Smart } from 'smart-webcomponents-angular/grid';
 import { IEmployeeModel } from './models/employee-model';
 import { DataService } from './service/data.service';
-import { IDepartmentModel } from './models/department-model';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  implements AfterViewInit, OnInit {
+export class AppComponent  implements OnInit {
   @ViewChild('grid', { read: GridComponent, static: false }) grid!: GridComponent;
 
   constructor(private dataService: DataService)  {}
 
   rowData: IEmployeeModel[] = [];
-  //departmentList: DepartmentModel[] = [];
   departmentList: string[] = [];
   selectedDepartment = '';
   
@@ -27,7 +24,7 @@ export class AppComponent  implements AfterViewInit, OnInit {
   }
 
   dataSource = new Smart.DataAdapter({
-      dataSource: this.rowData, //this.getEmployees1(),
+      dataSource: this.rowData, 
       dataFields: [
           'id: number',
           'firstName: string',
@@ -41,10 +38,11 @@ export class AppComponent  implements AfterViewInit, OnInit {
 
   columns = [
     { label: 'First Name', dataField: 'firstName', width: 150 },
-    { label: 'Last Name', dataField: 'lastName', width: 200 },
-    { label: 'Email', dataField: 'email', width: 150 },
-    { label: 'Phone', dataField: 'phone', width: 120 },
-    { label: 'Department', dataField: 'departmentName', width: 200, allowEdit: false }  
+    { label: 'Last Name', dataField: 'lastName', width: 150 },
+    { label: 'Email', dataField: 'email', width: 200 },
+    { label: 'Phone', dataField: 'phone', width: 150 },
+    { label: 'Department', dataField: 'departmentName', width: 250, allowEdit: false } ,
+    { label: 'Id', dataField: 'id', width: 20, allowEdit: false  },
   ]
 
   appearance = {
@@ -56,33 +54,9 @@ export class AppComponent  implements AfterViewInit, OnInit {
     action: 'click',
     mode: 'row'
   }
-  
-  getEmployees1() {
-    let rowData2: IEmployeeModel[] = [];
-    rowData2.push({ id: 1, firstName: 'alpha', lastName: 'betty', email: 'test@test.com', phone: '203-433-5677', departmentId: 2, departmentName: 'Human Resources'});
-    rowData2.push({ id: 2, firstName: 'alpha2', lastName: 'betty2', email: 'test@test.com2', phone: '203-433-5677-2', departmentId: 22, departmentName: 'Human Resources2'});
-
-    this.rowData = rowData2;
-    return this.rowData;
-  }
-
-  
-
 
   ngOnInit(): void {
-    
     this.getDepartments();
-
-    //  let rowData2: EmployeeModel[] = [];
-    //  rowData2.push({ id: 1, firstName: 'alpha', lastName: 'betty', email: 'test@test.com', phone: '203-433-5677', departmentId: 2, departmentName: 'Human Resources'});
-    //  rowData2.push({ id: 2, firstName: 'alpha2', lastName: 'betty2', email: 'test@test.com2', phone: '203-433-5677-2', departmentId: 22, departmentName: 'Human Resources2'});
-
-    //  this.rowData = rowData2;
-  }
-
-  ngAfterViewInit(): void {
-    // afterViewInit code.
-    this.init();
   }
 
   getDepartments() {
@@ -100,116 +74,93 @@ export class AppComponent  implements AfterViewInit, OnInit {
         })
   }
 
-init(): void {
-    // let rowData2: IEmployeeModel[] = [];
-    // rowData2.push({ id: 1, firstName: 'alpha', lastName: 'betty', email: 'test@test.com', phone: '203-433-5677', departmentId: 2, departmentName: 'Human Resources'});
-    // rowData2.push({ id: 2, firstName: 'alpha2', lastName: 'betty2', email: 'test@test.com2', phone: '203-433-5677-2', departmentId: 22, departmentName: 'Human Resources2'});
-    // this.rowData = rowData2;
+  selectDepartmentHandler(event: any) {
+    this.selectedDepartment =  event.target.value as string;
+  }
 
-    //this.getDepartments();
-}
+  getEmployees() {
+    this.dataService.getEmployeeList()
+      .subscribe(
+        (response) => { 
+          this.rowData = response;
+          this.dataSource = this.rowData
+        },
+        (error) => {  
+          console.error('Request failed with error')
+        },
+        () => {
+          //console.error('Request completed');      
+        })
+  }
 
-selectDepartmentHandler(event: any) {
-  this.selectedDepartment =  event.target.value as string;
-}
-
-getEmployees() {
-  this.dataService.getEmployeeList()
-    .subscribe(
-      (response) => { 
-        this.rowData = response;
-        this.dataSource = this.rowData
-        /*
-id: 1        
-firstName: "Bill"
-lastName: "Jones"
-email: "bill.jones@gmail.com"
-phone:"202-234-3445"
-departmentId: 1
-departmentName: "Accounting"
-
-
-rowData2.push({ id: 1, firstName: 'alpha', lastName: 'betty', email: 'test@test.com', phone: '203-433-5677', departmentId: 2, departmentName: 'Human Resources'});
-        */
-      },
-      (error) => {  
-        console.error('Request failed with error')
-      },
-      () => {
-        //console.error('Request completed');      
-      })
-}
-
-updateEmployee() {
-
-
-}
-
-addEmployee() {
-  let fName = (<HTMLInputElement>document.getElementById('firstName')).value;
-  let lName = (<HTMLInputElement>document.getElementById('lastName')).value;
-  let email = (<HTMLInputElement>document.getElementById('email')).value;
-  let phone = (<HTMLInputElement>document.getElementById('phone')).value;
-
-  const employee: IEmployeeModel = { firstName: fName, lastName: lName, email: email, phone: phone, departmentName: this.selectedDepartment, departmentId: 0, id: 0};
+  updateEmployee() {
+    let gridData: IEmployeeModel[] = [];
+    const grid = <Grid>document.getElementById('grid');
     
-    this.dataService.addEmployee(employee)
-    .subscribe(
-      (response) => {                           
-        let a = response;
-        /*
-        departmentId: 7
-        email: "gina.jones@tabasco.com"
-        firstName: "Gina"
-        id: 6
-        lastName: "Jones"
-        phone: "530-346-4545"
-        */
-        alert("Success. Employee Added to end of grid list");
-        this.resetAddEmployeeFields();
-      },
-      (error) => {                              
-        console.error('error caught in component')       
-        alert("Error adding Employee");
-        //throw error;   //You can also throw the error to a global error handler
+    let gridRows = grid.rows?.length;
+    if(gridRows !== undefined) {
+      
+      for(let i = 0; i < gridRows; i++) {
+        let fname =grid.getCellValue(i,"firstName");
+        let lname =grid.getCellValue(i,"lastName");
+        let email =grid.getCellValue(i,"email");
+        let phone =grid.getCellValue(i,"phone");
+        let department =grid.getCellValue(i,"departmentName");
+        let id =grid.getCellValue(i,"id");        
+
+        let employee: IEmployeeModel = {
+          id: id,
+          firstName: fname,
+          lastName: lname,
+          departmentName: department,
+          email: email,
+          phone: phone,
+          departmentId: 0
+        }
+
+        gridData.push(employee);
+        //UpdateEmployees
+      
       }
-    );  
-}
+    }
+  }
 
-private resetAddEmployeeFields() {
-  (<HTMLInputElement>document.getElementById('firstName')).value = "";
-  (<HTMLInputElement>document.getElementById('lastName')).value = "";
-  (<HTMLInputElement>document.getElementById('email')).value = "";
-  (<HTMLInputElement>document.getElementById('phone')).value = "";
-  (<HTMLInputElement>document.getElementById('masterDepartmentList')).value = "----";
-}
+  addEmployee() {
+    let fName = (<HTMLInputElement>document.getElementById('firstName')).value;
+    let lName = (<HTMLInputElement>document.getElementById('lastName')).value;
+    let email = (<HTMLInputElement>document.getElementById('email')).value;
+    let phone = (<HTMLInputElement>document.getElementById('phone')).value;
 
-private extractValues(arr: any, prop: string) {
-  let extractValues = arr.map((item: { [x: string]: any; }) => item[prop]);
-  Array.prototype.unshift.apply( extractValues, ["----"]);
-  this.departmentList = extractValues;
-} 
+    const employee: IEmployeeModel = { firstName: fName, lastName: lName, email: email, phone: phone, departmentName: this.selectedDepartment, departmentId: 0, id: 0};
+      
+      this.dataService.addEmployee(employee)
+      .subscribe(
+        (response) => {                           
+          alert("Success. Employee Added to end of grid list");
+          this.resetAddEmployeeFields();
+          this.getEmployees();
+        },
+        (error) => {                              
+          console.error('error caught in component')       
+          alert("Error adding Employee");
+          //throw error;   //You can also throw the error to a global error handler
+        }
+      );  
+  }
 
+  private resetAddEmployeeFields() {
+    (<HTMLInputElement>document.getElementById('firstName')).value = "";
+    (<HTMLInputElement>document.getElementById('lastName')).value = "";
+    (<HTMLInputElement>document.getElementById('email')).value = "";
+    (<HTMLInputElement>document.getElementById('phone')).value = "";
+    (<HTMLInputElement>document.getElementById('masterDepartmentList')).value = "----";
+  }
 
-// Get the row here.
-// grid.addEventListener('beginEdit', (event) => {  
-  
-//   const detail = event.detail;
-//   const id = detail.id;
-//   const dataField = detail.dataField;
-//   const row = detail.row;
-//   const column = detail.column;
-//   const cell = detail.cell;
-//   const data = detail.data;
-//   const value = detail.value;
-
-//   if(id % 2 === 0) {
-//     grid.cancelEdit()
-//   }
-
-// })
-
-
+  private extractValues(arr: any, prop: string) {
+    let extractValues = arr.map((item: { [x: string]: any; }) => item[prop]);
+    Array.prototype.unshift.apply( extractValues, ["----"]);
+    this.departmentList = extractValues;
+  } 
 
 }
 
